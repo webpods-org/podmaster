d40675339f739c1109586d109d66407113f604ac
Jeswin Kumar <jeswinpk@agilehead.com>
2021-04-28 12:07:24 +0530

    Initial commit

--- db/drop-tables.sql
+++ db/drop-tables.sql
@@ -1,15 +1,1 @@
-DROP TABLE IF EXISTS "provider_user";
-
-DROP TABLE IF EXISTS "user_token";
-
-DROP TABLE IF EXISTS "role_token";
-
-DROP TABLE IF EXISTS "user_role";
-
-DROP TABLE IF EXISTS "local_user_auth";
-
-DROP TABLE IF EXISTS "user";
-
-DROP TABLE IF EXISTS "role";
-
-DROP TABLE IF EXISTS "kvstore";
+DROP TABLE IF EXISTS "log";
